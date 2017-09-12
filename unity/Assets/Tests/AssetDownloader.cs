f795a55e2d3bd57a0b7bc9df1e575dc188ea720d
POPEYE\diego <diegomazala@gmail.com>
2017-09-12 19:59:54 -0300

    Update unity version; Loading Lu character

--- unity/Assets/Tests/AssetDownloader.cs
+++ unity/Assets/Tests/AssetDownloader.cs
@@ -8,10 +8,14 @@ class AssetDownloader : MonoBehaviour
     public string GameObjectAssetName = "cube";
     public string SkyboxAssetBundleUrl = "http://localhost:8012/unity/bundles/skyboxes/skybox.unity3d";
     public string SkyboxAssetName = "sea";
+    public string SceneAssetBundleUrl = "http://localhost:8012/unity/bundles/stage_scene/stage.unity3d";
+    public string ScenesAssetName = "scene.unity3d";
+    public string[] ScenesAssetPaths;
 
     public GameObject assetInstantiated;
 
     private AssetBundle assetBundle = null;
+    
 
     void OnDisable()
     {
@@ -27,6 +31,11 @@ class AssetDownloader : MonoBehaviour
     public void DownloadSkybox()
     {
         StartCoroutine(GetAssetBundle_Skybox(SkyboxAssetBundleUrl, SkyboxAssetName));
+    }
+
+    public void DownloadScenes()
+    {
+        StartCoroutine(GetAssetBundle_Scene(SceneAssetBundleUrl, ScenesAssetName));
     }
 
 
@@ -66,6 +75,26 @@ class AssetDownloader : MonoBehaviour
         {
             assetBundle = DownloadHandlerAssetBundle.GetContent(www);
             assetInstantiated = Instantiate(assetBundle.LoadAsset<GameObject>(assetName));
+            assetBundle.Unload(false);
+        }
+
+    }
+
+
+    IEnumerator GetAssetBundle_Scene(string assetBundleUrl, string assetName)
+    {
+        UnityWebRequest www = UnityWebRequest.GetAssetBundle(assetBundleUrl);
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            assetBundle = DownloadHandlerAssetBundle.GetContent(www);
+            ScenesAssetPaths = assetBundle.GetAllScenePaths();
+            //assetInstantiated = Instantiate(assetBundle.LoadAsset<GameObject>(assetName));
             assetBundle.Unload(false);
         }
 
